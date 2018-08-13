6847ded1ca50abbd875897b71dbfbbbb07bf48b4
Ainruiz <37852053+Ainruiz@users.noreply.github.com>
2018-08-13 19:25:35 -0300

    spotify service update and style of search html

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -8,26 +8,25 @@ export class SpotifyService {
   
   constructor(private http: HttpClient) {
     console.log("spotify service im here!!!!1");
-
   }
   
   getNewPlaylist() {
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBSEpwn2-rvGPAr6XyWkHidWU4IZWGHe5z81ZbvL9QNvyS-d7kCRNfbAMXrtLfYQ7J-AzHcZfcTQFQmB2A'   
+      'Authorization': 'Bearer BQArMfksK6YmbM_p4MbuK2yz0JMzSDoYNFAwdXQ3eh0rHjhM0CTIjy4yJgVxVOm4CelZdAT0eYOBdmYovR4'   
     });
     return this.http.get('https://api.spotify.com/v1/browse/featured-playlists', {headers});
 
   }
   getSearchArtist( query:string) {
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBSEpwn2-rvGPAr6XyWkHidWU4IZWGHe5z81ZbvL9QNvyS-d7kCRNfbAMXrtLfYQ7J-AzHcZfcTQFQmB2A'   
+      'Authorization': 'Bearer BQArMfksK6YmbM_p4MbuK2yz0JMzSDoYNFAwdXQ3eh0rHjhM0CTIjy4yJgVxVOm4CelZdAT0eYOBdmYovR4'   
     });
     return this.http.get(`https://api.spotify.com/v1/search?q=${query}&type=track%2Cartist&limit=1`, {headers});
   }
 
   getSearchTrack(query:string){
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBSEpwn2-rvGPAr6XyWkHidWU4IZWGHe5z81ZbvL9QNvyS-d7kCRNfbAMXrtLfYQ7J-AzHcZfcTQFQmB2A'   
+      'Authorization': 'Bearer BQArMfksK6YmbM_p4MbuK2yz0JMzSDoYNFAwdXQ3eh0rHjhM0CTIjy4yJgVxVOm4CelZdAT0eYOBdmYovR4'   
     });
     return this.http.get(`https://api.spotify.com/v1/search?q=${query}&type=track&limit=3`, {headers});
   }
